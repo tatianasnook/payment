98b7bef18e6a754eb4900ac37c06d51e3e55206a
tatianasnook <tatsnook@gmail.com>
2024-04-04 19:19:14 -0500

    added front page in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import StripeContainer from "./Stripe/StripeContainer";
+import strawberries from './strawberries.jpg';
 
 const App = () => {
-  
+  const [ showMyImage, setShowMyImage ] = useState(false);
+
   return (
     <div className="App">
-      <StripeContainer />
+      {showMyImage ? <StripeContainer /> : 
+        <div>
+          
+          <p>FRUIT SHOP</p>
+          <p>$9.99</p>
+          <img src={strawberries} alt='box of strawberries' width="500"/>
+          <div>
+            <button onClick={() => setShowMyImage(true)}>Buy some strawberries!</button>
+          </div>
+        
+        </div>
+      }
+    
     </div>
   );
 };
